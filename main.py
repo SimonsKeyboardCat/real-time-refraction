284780ba4a7fb8cae30c17d05feb2ea51d8d8f35
Marcin Szymczak <92090671+maruchiso@users.noreply.github.com>
2025-01-30 21:29:17 +0100

    final version

--- main.py
+++ main.py
@@ -2,60 +2,70 @@ from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import Shader, Texture, FrameBufferProperties, WindowProperties
 
-#folder z deafultowymi modelami
-#import panda3d
-#print(panda3d.__file__)
-
 class App(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        
-        #środowisko - tło
-        self.scene = self.loader.loadModel("models/environment") #wgranie wbudowanego modelu
-        self.scene.reparentTo(self.render) #przypisanie do wezla render
-        self.scene.setScale(0.25, 0.25, 0.25) #skalowanie
-        self.scene.setPos(-8, 42, 0) #ustawianie w przestrzeni
 
-        #obiekt refrakcyjny
-        #models/misc/sphere - nie działa tylko kolor??, models/box - działa
-        self.refractive_object = self.loader.loadModel("models/panda") #wgranie wbudowanego modelu 
-        self.refractive_object.reparentTo(self.render) #przypisanie do wezla render
-        self.refractive_object.setScale(2) #powiekszenie obiektu 
-        self.refractive_object.setPos(0, 10, 3) #ustawenie w przestrzeni
+        #Background
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
 
-        #renderowanie geometrii i głębi do tekstury
-        fb_props = FrameBufferProperties() #tworzymy bufor klatki
-        fb_props.setRgbColor(True) #bufor przechowuje informacje na temat koloru klatki RGB
-        fb_props.setDepthBits(1) #ilość bitów na przechowanie informacji na temat głębokości 
-        win_props = WindowProperties.size(512, 512)  #rozdzielczość tekstury 
-        #bufor wyjsciowy
+        #Refraction
+        self.refractive_object = self.loader.loadModel("models/panda")
+        self.refractive_object.reparentTo(self.render)
+        self.refractive_object.setScale(2)
+        self.refractive_object.setPos(0, 10, 3)
+
+        #Buffer for Depth and Color
+        fb_props = FrameBufferProperties()
+        fb_props.setRgbColor(True)
+        fb_props.setDepthBits(1)
+        win_props = WindowProperties.size(512, 512)
+
+        #Buffer main scene
         self.buffer = self.graphicsEngine.makeOutput(
             self.pipe, "offscreen buffer", -2, fb_props, win_props,
             GraphicsPipe.BFRefuseWindow, self.win.getGsg(), self.win)
 
-        self.color_texture = Texture() #tekstura przechowująca kolor
-        self.depth_texture = Texture() #tekstura przechowująca głębie
-        #dodanie tekstur do bufora
+        self.color_texture = Texture()
+        self.depth_texture = Texture()
         self.buffer.addRenderTexture(self.color_texture, GraphicsOutput.RTMCopyRam)
         self.buffer.addRenderTexture(self.depth_texture, GraphicsOutput.RTMBindOrCopy, GraphicsOutput.RTPDepth)
 
-        #kopia sceny do hidden_scene
+        #Near geometry buffer
+        self.near_geometry_buffer = self.graphicsEngine.makeOutput(
+            self.pipe, "near geometry buffer", -2, fb_props, win_props,
+            GraphicsPipe.BFRefuseWindow, self.win.getGsg(), self.win)
+
+        self.near_geometry_color_texture = Texture()
+        self.near_geometry_depth_texture = Texture()
+        self.near_geometry_buffer.addRenderTexture(self.near_geometry_color_texture, GraphicsOutput.RTMCopyRam)
+        self.near_geometry_buffer.addRenderTexture(self.near_geometry_depth_texture, GraphicsOutput.RTMBindOrCopy, GraphicsOutput.RTPDepth)
+
+        #Camera for hidden scene (depth rendering)
         self.hidden_scene = NodePath("Hidden Scene")
         self.scene.instanceTo(self.hidden_scene)
-
-        #kamera do hidden_scene
         self.buffer_cam = self.makeCamera(self.buffer, scene=self.hidden_scene)
         self.buffer_cam.reparentTo(self.cam)
 
-        self.refractive_index = 1.5  #TODO: przyjąć input od usera
-        #Załadowanie shaderów
+        #Camera for near geometry
+        self.near_geometry_scene = NodePath("Near Geometry Scene")
+        self.scene.instanceTo(self.near_geometry_scene)
+        self.buffer_cam_near_geom = self.makeCamera(self.near_geometry_buffer, scene=self.near_geometry_scene)
+
+        #Refractive index
+        self.refractive_index = 1.5
+
+        #Shaders
         self.refractive_object.setShader(Shader.load(Shader.SLGLSL, vertex="shaders/refraction.vert", fragment="shaders/refraction.frag"))
         self.refractive_object.setShaderInput("color_texture", self.color_texture)
         self.refractive_object.setShaderInput("depth_texture", self.depth_texture)
-        self.refractive_object.setShaderInput("camera_nearfar", Vec2(self.camLens.getNear(), self.camLens.getFar()))#odległość kamery od najbliższego obiektu i najdalszego obiektu 
+        self.refractive_object.setShaderInput("near_geometry_color", self.near_geometry_color_texture)
+        self.refractive_object.setShaderInput("near_geometry_depth", self.near_geometry_depth_texture)
+        self.refractive_object.setShaderInput("camera_nearfar", Vec2(self.camLens.getNear(), self.camLens.getFar()))
         self.refractive_object.setShaderInput("refractive_index", self.refractive_index)
 
-
-#uruchomienie głównej pętli        
 app = App()
 app.run()
--- shaders/refraction.frag
+++ shaders/refraction.frag
@@ -1,22 +1,55 @@
 #version 130
 uniform sampler2D color_texture;
 uniform sampler2D depth_texture;
+uniform sampler2D near_geometry_color;
+uniform sampler2D near_geometry_depth;
 uniform vec2 camera_nearfar;
 uniform float refractive_index;
+
 in vec2 texcoord;
 out vec4 fragColor;
+
+const int NUM_SAMPLES = 7;
+const float min_error = 1e-2; 
+const int max_iterations = 5; 
+
 void main() {
     float scene_depth = texture(depth_texture, texcoord).r;
     float near = camera_nearfar.x;
     float far = camera_nearfar.y;
     float z = (2.0 * near * far) / (far + near - (2.0 * scene_depth - 1.0) * (far - near));
+
     vec2 refracted_coords = texcoord;
-    for (int i = 0; i < 5; ++i) {
-        float new_depth = texture(depth_texture, refracted_coords).r;
+    vec3 final_color = vec3(0.0);
+    
+    vec2 best_coords = refracted_coords;
+    float min_depth_error = 1e6;
+
+    //Multiple depth sampling along the refracted ray
+    for (int j = 0; j < NUM_SAMPLES; ++j) {
+        vec2 sample_coords = texcoord + j * (refracted_coords - texcoord) / float(NUM_SAMPLES);
+        float sampled_depth = texture(near_geometry_depth, sample_coords).r;
+        float sampled_z = (2.0 * near * far) / (far + near - (2.0 * sampled_depth - 1.0) * (far - near));
+
+        float depth_error = abs(sampled_z - z);
+        if (depth_error < min_depth_error) {
+            min_depth_error = depth_error;
+            best_coords = sample_coords;
+        }
+    }
+
+    //Starting iteration from the best point
+    refracted_coords = best_coords;
+    for (int i = 0; i < max_iterations; ++i) {
+        float new_depth = texture(near_geometry_depth, refracted_coords).r;
         float new_z = (2.0 * near * far) / (far + near - (2.0 * new_depth - 1.0) * (far - near));
-        if (abs(new_z - z) < 0.01) break;
+
+        if (abs(new_z - z) < min_error) {
+            final_color = texture(near_geometry_color, refracted_coords).rgb;
+            break;
+        }
         refracted_coords += vec2(0.01, 0.01) * (1.0 / refractive_index);
     }
-    vec4 color = texture(color_texture, refracted_coords);
-    fragColor = color;
+
+    fragColor = vec4(final_color, 1.0);
 }
